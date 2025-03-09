b6027b45252d351c925f7d8430bb16251812a6ca
asimov <seyranmustafa4@gmail.com>
2025-03-09 22:00:35 +0300

    p

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,10 +13,10 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
-// import authRoutes from "./routes/auth.route.js";
-// import songRoutes from "./routes/song.route.js";
-// import albumRoutes from "./routes/album.route.js";
-// import statRoutes from "./routes/stat.route.js";
+import authRoutes from "./routes/auth.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
 
 dotenv.config();
 
