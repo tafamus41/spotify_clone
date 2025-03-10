e317cd5af3ce61a531eeb9fd7cd57a77246d8cd4
asimov <seyranmustafa4@gmail.com>
2025-03-11 02:53:47 +0300

    p

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,8 @@ import ChatPage from "./pages/chat/ChatPage";
 import AlbumPage from "./pages/album/AlbumPage";
 import AdminPage from "./pages/admin/AdminPage";
 import { Toaster } from "react-hot-toast";
-import NotFoundPage from "./pages/404/NotFoundPage";
 import HomePage from "./pages/home/HomePage";
+import NotFoundPage from "./pages/404/NotFoundPage";
 
 
 function App() {
