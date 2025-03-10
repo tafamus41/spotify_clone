7dc8f53fc751ed4d4b87dffee36f8a09588a6184
asimov <seyranmustafa4@gmail.com>
2025-03-10 07:31:55 +0300

    p

--- frontend/src/pages/auth-callback/AuthCallbackPage.tsx
+++ frontend/src/pages/auth-callback/AuthCallbackPage.tsx
@@ -1,7 +1,7 @@
-import { Card, CardContent } from "@/components/ui/card";
+// import { Card, CardContent } from "@/components/ui/card";
 import { axiosInstance } from "@/lib/axios";
 import { useUser } from "@clerk/clerk-react";
-import { Loader } from "lucide-react";
+// import { Loader } from "lucide-react";
 import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -35,13 +35,13 @@ const AuthCallbackPage = () => {
 
 	return (
 		<div className='h-screen w-full bg-black flex items-center justify-center'>
-			<Card className='w-[90%] max-w-md bg-zinc-900 border-zinc-800'>
+			{/* <Card className='w-[90%] max-w-md bg-zinc-900 border-zinc-800'>
 				<CardContent className='flex flex-col items-center gap-4 pt-6'>
 					<Loader className='size-6 text-emerald-500 animate-spin' />
 					<h3 className='text-zinc-400 text-xl font-bold'>Logging you in</h3>
 					<p className='text-zinc-400 text-sm'>Redirecting...</p>
 				</CardContent>
-			</Card>
+			</Card> */}
 		</div>
 	);
 };
