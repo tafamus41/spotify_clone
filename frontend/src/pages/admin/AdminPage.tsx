27876cadc323c941a0ec37713762c88ad2b14269
asimov <seyranmustafa4@gmail.com>
2025-03-11 03:26:04 +0300

    p

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -2,11 +2,11 @@ import { useAuthStore } from "@/stores/useAuthStore";
 import Header from "./components/Header";
 import DashboardStats from "./components/DashboardStats";
 import { Album, Music } from "lucide-react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SongsTabContent from "./components/SongsTabContent";
 import AlbumsTabContent from "./components/AlbumsTabContent";
 import { useEffect } from "react";
 import { useMusicStore } from "@/stores/useMusicStore";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const AdminPage = () => {
 	const { isAdmin, isLoading } = useAuthStore();
